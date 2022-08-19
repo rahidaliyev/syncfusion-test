41320f8861d897a50590bdc6b66377f86ca399c8
rahidaliyev <rahidaliyev18@gmail.com>
2022-08-19 21:31:25 +0400

    table updated

--- src/components/Table.js
+++ src/components/Table.js
@@ -14,11 +14,11 @@ export default function  CommandColumnEdit(){
         <div className='control-section'>
           <GridComponent  dataSource={data} id='gridcomp'  allowPaging={true} pageSettings={{ pageCount: 5 }} editSettings={editSettings}>
             <ColumnsDirective>
-              <ColumnDirective field='taskId' headerText='Task ID' width='120' textAlign='Center' isPrimaryKey={true} validationRules={validationRule}></ColumnDirective>
+              {/* <ColumnDirective field='taskId' headerText='Task ID' width='120' textAlign='Center' isPrimaryKey={true} validationRules={validationRule}></ColumnDirective> */}
               <ColumnDirective field='taskName' headerText='Task Name' width='150' validationRules={validationRule}></ColumnDirective>
-              <ColumnDirective field='taskStatus' headerText='Task Status'   width='120'  textAlign='Center' editType='numericedit'></ColumnDirective>
-              <ColumnDirective field='parentId' headerText='Parent ID' editType='defaultEdit' format='yMd' width='170'></ColumnDirective>
-              <ColumnDirective headerText='Manage Records' width='160' commands={commands}></ColumnDirective>
+              <ColumnDirective field='taskStatus' headerText='Task Status'   width='120'   textAlign='Center' editType='numericedit'></ColumnDirective>
+              {/* <ColumnDirective field='parentId' headerText='Parent ID' editType='defaultEdit' format='yMd' width='170'></ColumnDirective> */}
+              <ColumnDirective headerText='Actions' width='160' commands={commands}></ColumnDirective>
             </ColumnsDirective>
             <Inject services={[Page, CommandColumn, Edit]}/>
           </GridComponent>
