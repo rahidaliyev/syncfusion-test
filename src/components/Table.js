5209c22fc7f64758df6e39419b481c2ba270b1c0
rahidaliyev <rahidaliyev18@gmail.com>
2022-08-20 23:27:06 +0400

    validation added

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,8 +3,10 @@ import { GridComponent, ColumnsDirective, ColumnDirective, Inject, Page, Edit, C
 import  data  from '../data.json';
 export default function Table(){
 
-    const editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true, allowEditOnDblClick: false,showDeleteConfirmDialog: true };
-    const validationRule = { required: true };
+    const editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true, allowEditOnDblClick: false,showDeleteConfirmDialog: true, mode: 'Dialog'};
+    const validationRule = { required: true};
+    const validRule={required:true,range:[0,1]}
+    
     const commands = [
         { type: 'Edit', buttonOption: { iconCss: 'e-icons e-edit', cssClass: 'e-flat-edit' } },
         { type: 'Delete', buttonOption: { iconCss: 'e-icons e-delete', cssClass: 'e-flat-delete' } },
@@ -15,9 +17,9 @@ export default function Table(){
         <div className='control-section'>
           <GridComponent  dataSource={data} id='gridcomp'  allowPaging={true} pageSettings={{ pageCount: 5 }} editSettings={editSettings}>
             <ColumnsDirective>
-              {/* <ColumnDirective field='taskId' headerText='Task ID' width='120' textAlign='Center' isPrimaryKey={true} validationRules={validationRule}></ColumnDirective> */}
               <ColumnDirective field='taskName' headerText='Task Name' width='150' validationRules={validationRule}></ColumnDirective>
-              <ColumnDirective field='taskStatus' headerText='Task Status'   width='120'   textAlign='Center' editType='numericedit'></ColumnDirective>
+              <ColumnDirective field='taskStatus' headerText='Task Status'   width='120'   textAlign='Center' editType='numericedit' validationRules={validRule}></ColumnDirective>
+              {/* <ColumnDirective field='taskId' headerText='Task ID' width='120' textAlign='Center' isPrimaryKey={true} validationRules={validationRule}></ColumnDirective> */}
               {/* <ColumnDirective field='parentId' headerText='Parent ID' editType='defaultEdit' format='yMd' width='170'></ColumnDirective> */}
               <ColumnDirective headerText='Actions' width='160' commands={commands}></ColumnDirective>
             </ColumnsDirective>
